e29bf95dcca190b5e47cac3898531a56430ebcd8
SebastienBoisard <sebastien.boisard@gmail.com>
2016-08-04 17:29:04 +0200

    Add Gin-Gonic as a router

--- server.go
+++ server.go
@@ -4,22 +4,36 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the HomePage!")
+func homePage(context *gin.Context) {
+	context.String(http.StatusOK, "Welcome to the HomePage!")
 	fmt.Println("Endpoint: homePage")
 }
 
-func testEndPoint(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "test")
+func testEndPoint(context *gin.Context) {
+	id := context.DefaultQuery("id", "id void")
+	name := context.DefaultQuery("name", "name void")
+
+	context.String(http.StatusOK, "test id=%s  name=%s", id, name)
+
 	fmt.Println("Endpoint: test")
 }
 
 func handleRequests() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/test", testEndPoint)
-	err := http.ListenAndServeTLS(":8443", "server.crt", "server.key", nil)
+
+	// Instantiate a new router
+	router := gin.Default()
+
+	// Add a handler on /
+	router.GET("/", homePage)
+
+	// Add a handler on /test
+	router.GET("/test", testEndPoint)
+
+	err := router.Run(":8000")
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
